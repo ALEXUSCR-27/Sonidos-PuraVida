bb33404b93d93b8aa623cf15d0cd10e4fa3ea0c3
Alexander Sánchez Cespedes <asmalexander@hotmail.com>
2023-11-20 01:56:18 -0600

    Creditos

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,6 +8,10 @@ import '../styles/general.css';
 export default function Footer() {
     return (
         <div className="footer">
+            <div className="credits">
+                <p>Designed by <a className="author" href="http://www.linkedin.com/in/alexander-sánchez-cespedes-b660b721b">ALEXUSCR</a>
+                </p>
+            </div>
         </div>
         
 
